272a65d999fcc5587f10363011320b16c26eae45
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-20 16:40:50 -0700

    Feature: add working original prototype

---
+++ README.md
@@ -0,0 +1,7 @@
+## About
+
+Menu with SVG animated blobs.
+
+## Creadits
+
+[Navigation](https://codepen.io/v_Bauer/pen/WNroMOq)
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,63 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import gsap from "gsap";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    function move(id: string, position: string, color: string) {
+        var tl = gsap.timeline();
+        tl
+            .to("#bgBubble", { duration: 0.15, bottom: "-30px", ease: "ease-out" }, 0)
+            .to("#bubble1", { duration: 0.1, y: "120%", boxShadow: 'none', ease: "ease-out", }, 0)
+            .to("#bubble2", { duration: 0.1, y: "120%", boxShadow: 'none', ease: "ease-out", }, 0)
+            .to("#bubble3", { duration: 0.1, y: "120%", boxShadow: 'none', ease: "ease-out", }, 0)
+            .to("#bubble4", { duration: 0.1, y: "120%", boxShadow: 'none', ease: "ease-out", }, 0)
+            .to(".icon", { duration: 0.05, opacity: 0, ease: "ease-out", }, 0)
+            .to("#bgBubble", { duration: 0.2, left: position, ease: "ease-in-out" }, 0.1)
+            .to("#bgBubble", { duration: 0.15, bottom: "-50px", ease: "ease-out" }, '-=0.2')
+            .to(`#bubble${id}`, { duration: 0.15, y: "0%", opacity: 1, boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)', ease: "ease-out" }, '-=0.1')
+            .to(`#bubble${id}> span`, { duration: 0.15, y: "0%", opacity: 0.7, ease: "ease-out" }, '-=0.1')
+            .to("#navbarContainer", { duration: 0.3, backgroundColor: color, ease: "ease-in-out" }, 0)
+            .to("#bg", { duration: 0.3, backgroundColor: color, ease: "ease-in-out" }, 0)
+            .to("#bgBubble", { duration: 0.3, backgroundColor: color, ease: "ease-in-out" }, 0);
+    }
+
+    return (
+        <div className="App">
+            <div id="navbarContainer">
+                <div id="navbar">
+                    <div id="bubbleWrapper">
+                        <div id="bubble1" className="bubble"><span className="icon"><i className="fas fa-home"></i></span></div>
+                        <div id="bubble2" className="bubble"><span className="icon"><i className="fab fa-twitter"></i></span></div>
+                        <div id="bubble3" className="bubble"><span className="icon"><i className="fas fa-bell"></i></span></div>
+                        <div id="bubble4" className="bubble"><span className="icon"><i className="fas fa-user"></i></span></div>
+                    </div>
+                    <div id="menuWrapper">
+                        <div id="menu1" className="menuElement" onClick={() => move('1', '50px', '#ffcc80')}><i className="fas fa-home"></i></div>
+                        <div id="menu2" className="menuElement" onClick={() => move('2', '150px', '#81d4fa')}><i className="fab fa-twitter"></i></div>
+                        <div id="menu3" className="menuElement" onClick={() => move('3', '250px', '#c5e1a5')}><i className="fas fa-bell"></i></div>
+                        <div id="menu4" className="menuElement" onClick={() => move('4', '350px', '#ce93d8')}><i className="fas fa-user"></i></div>
+                    </div>
+                </div>
+                <div id="bgWrapper">
+                    <div id="bg"></div>
+                    <div id="bgBubble"></div>
+                </div>
+            </div>
+
+            <svg width="0" height="0" >
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" id="blurFilter" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -15" result="goo" />
+                        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+                    </filter>
+                </defs>
+            </svg>
+
+        </div>
+    );
 }
 
-export default App
+export default App;
